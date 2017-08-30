9fd8eb551b8bf62201e05c37caf7b046ba823e60
KyleJohnson <kylejohnson2612@gmail.com>
2017-08-29 19:30:19 -0600

    Edited http request form

--- kyle/assets/php/recieved.php
+++ kyle/assets/php/recieved.php
@@ -11,7 +11,7 @@ $comments = $_POST[comments];
 //Write the Email
 $to = 'kylejohnson2612@gmail.com';
 $subject = 'Requst Info';
-$message = "Thank you $name for requesting information about $service from Kyle Design & Development. A representative will contact you shortly.";
+$message = "Thank you $name, for requesting information about $service from Kyle Design & Development. A representative will contact you shortly. This form was sent by HTTP request.";
 
 //Send the Email
 mail($to, $subject, $message, 'FROM:'.$email);
@@ -22,16 +22,40 @@ mail($to, $subject, $message, 'FROM:'.$email);
 <html>
 <head>
 <meta charset="utf-8">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<link rel="stylesheet" href="styles.css" >
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-<title>Sent Email</title>
+<link rel="stylesheet" href="../css/styles.css" >
+<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+
+<title>Sent</title>
 </head>
 
-<body>
-<?php  echo $message; ?>
-<p>Your contact information is:</p>
-<?php echo $phone; ?><br />
-<?php echo $email; ?><br />
+<body id="requestPage">
+<div class="container fade-in">
+	<div class="row">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-10 demobox">
+		  <div class="row">
+		  	<div class="col-sm-12">
+                <h2>Thank You!</h2>
+                <p><?php  echo $message; ?></p>
+                <p>Your contact information is:</p>
+                <p><?php echo $phone; ?></p>
+                <p><?php echo $email; ?></p>
+                <p><a href="index.html">Send another form!</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-1"></div>
+    </div>
+</div>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
