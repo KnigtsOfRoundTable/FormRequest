c60542004c0129d0c2e658e9f7be3ee069013ecc
Erick Perez <NBA_EAP@hotmail.com>
2017-08-31 13:17:50 -0600

    Add Folders and update erickthanks.php

--- erickThanks.php
+++ erickThanks.php
@@ -1,3 +1,21 @@
+<?php
+  $name = $_REQUEST['name'];
+  $from = $_REQUEST['from'];
+  $body = $_REQUEST['body'];
+
+  //$to = "hiro@mrhiro.com"; //recipient 
+$to = "NBA_EAP@hotmail.com";
+
+  $subject = "RIA Emails"; //subject 
+  $header = "From: ". $name . " <" . $from . ">\r\n";
+
+  if (mail($to, $subject, $body, $header)){
+    echo 'Your email has been sent!';
+  } else {
+    echo 'Error: something went wrong.';
+  }
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
